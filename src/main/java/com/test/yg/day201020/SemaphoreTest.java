0ef495367804bf8e0acab2717feb6578b3cfd892
shine <361596037@qq.com>
2020-10-20 21:29:32 +0800

    remove uneless config

--- src/main/java/com/test/yg/day201020/SemaphoreTest.java
+++ src/main/java/com/test/yg/day201020/SemaphoreTest.java
@@ -1,6 +1,9 @@
 package com.test.yg.day201020;
 
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 控制访问资源线程数目
@@ -11,8 +14,9 @@ public class SemaphoreTest {
     public static void main(String[] args) {
         // 表明一次只能有两个线程同时在执行
         Semaphore semaphore = new Semaphore(2);
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5,5,60L, TimeUnit.SECONDS, new LinkedBlockingDeque<>());
         for (int i = 0; i < 5; i++) {
-            new Thread(()-> {
+            threadPoolExecutor.execute(() -> {
                 try {
                     semaphore.acquire();
                     System.out.println("开始处理线程名: " + Thread.currentThread().getName() + ", at time:" + System.currentTimeMillis());
@@ -22,7 +26,7 @@ public class SemaphoreTest {
                 } finally {
                     semaphore.release();
                 }
-            },"t" + i).start();
+            });
         }
     }
 
