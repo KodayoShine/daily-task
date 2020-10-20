b4cb1b87ebf51f063b1867be06a6105abcf8731f
shine <361596037@qq.com>
2020-10-20 21:43:53 +0800

    CountDownLatch代码 补充和完善解释说明

---
+++ src/main/java/com/test/yg/day201020/CountDownLatchTest.java
@@ -0,0 +1,52 @@
+package com.test.yg.day201020;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 可以使其他线程完成各自任务后再一同执行的工具类
+ *
+ * 使用场景:
+ *      应用程序的主线程在启动框架之前,需要保证负责启动的线程全都完成,再向下执行
+ *
+ * 简单描述工作原理:
+ *      通过state字段进行表示
+ *      其余线程调用countDown的时候,都会将state字段使用CAS做减一操作
+ *      主线程的await,则会自旋的查看state是否为0
+ *
+ */
+public class CountDownLatchTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        new Thread(() ->{
+            try {
+                System.out.println("准备洗漱---");
+                Thread.sleep(1500);
+                System.out.println("洗漱完成---");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                countDownLatch.countDown();
+            }
+        }).start();
+
+
+        new Thread(() ->{
+            try {
+                System.out.println("准备做饭---");
+                Thread.sleep(3000);
+                System.out.println("做饭完成---");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                countDownLatch.countDown();
+            }
+        }).start();
+
+        countDownLatch.await();
+        System.out.println("早上结束啦");
+
+    }
+
+}
--- src/main/java/com/test/yg/day201020/SemaphoreTest.java
+++ src/main/java/com/test/yg/day201020/SemaphoreTest.java
@@ -8,6 +8,14 @@ import java.util.concurrent.TimeUnit;
 /**
  * 控制访问资源线程数目
  *
+ * 使用场景:
+ *      主要用于资源访问,服务限流
+ *
+ * 简单描述工作原理:
+ *      AQS中的state进行计数操作,每次调用acquire方法时,则进行CAS原子级的操作,将state做减一操作
+ *      当state为0,也就是资源已经全部被占用的时候,多余的线程会进入到AQS的同步队列中,进行阻塞(这里面使用UNSFATE.PARK进行阻塞)等待
+ *      当有任意线程释放release的时候,就会调用头部节点的下一个节点,进行唤醒操作
+ *      
  */
 public class SemaphoreTest {
 
