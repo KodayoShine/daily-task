ec1351a8c1dc1227738ce2b57a42b8c30caf4043
shine <361596037@qq.com>
2020-10-24 23:55:06 +0800

    原子更新字段类AtomicIntegerFieldUpdater

--- src/main/java/com/test/yg/day201024/AtomicArrayTest.java
+++ src/main/java/com/test/yg/day201024/AtomicArrayTest.java
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicIntegerArray;
  * 原子数组测试
  * 可以原子性的修改数组
  * AtomicIntegerArray的构造方法,是将原本的数组进行拷贝
- *
+ * 这样原本数组的对应索引的值,和新对象的值 不一样
  *
  */
 public class AtomicArrayTest {
@@ -21,9 +21,13 @@ public class AtomicArrayTest {
 
         int firstValue = atomicIntegerArray.get(0);
         System.out.println(firstValue);
+        int originalValue = arr[0];
+        System.out.println("比较:" + (firstValue == originalValue));
 
         int nextValue = atomicIntegerArray.incrementAndGet(0);
         System.out.println(nextValue);
+        
+        
     }
 
 
---
+++ src/main/java/com/test/yg/day201024/AtomicIntegerFieldUpdaterTest.java
@@ -0,0 +1,34 @@
+package com.test.yg.day201024;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+/**
+ * 原子更新引用类型
+ * 原子更新字段类都是抽象类，每次使用都时候必须使用静态方法newUpdater创建一个
+ * 更新器。原子更新类的字段的必须使用public volatile修饰符。
+ *
+ * 使用AtomicIntegerFieldUpdater类进行原子操作的字段,必须使用volatile进行标志
+ *
+ *
+ */
+public class AtomicIntegerFieldUpdaterTest {
+
+    static AtomicIntegerFieldUpdater atomicIntegerFieldUpdater = AtomicIntegerFieldUpdater.newUpdater(Person.class, "age");
+
+    public static void main(String[] args) {
+        Person person = new Person(18, "shine");
+        atomicIntegerFieldUpdater.getAndIncrement(person);
+        System.out.println(person.age);
+    }
+
+}
+
+class Person {
+    volatile int age;
+    String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
