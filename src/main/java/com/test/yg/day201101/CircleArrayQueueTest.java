1844de9abebaeb2f1f90b33a9032620b02470db7
shine <361596037@qq.com>
2020-11-01 22:56:08 +0800

    修改循环队列数组中遍历数据的计算公式,计算当前队列有效数据 添加查询头部方法

--- src/main/java/com/test/yg/day201101/CircleArrayQueueTest.java
+++ src/main/java/com/test/yg/day201101/CircleArrayQueueTest.java
@@ -13,6 +13,11 @@ public class CircleArrayQueueTest {
         circleArray.add(4);
         circleArray.add(5);
         circleArray.showAll();
+
+        int i = circleArray.get();
+        System.out.printf("查询当前头部数值%d\n", i);
+        circleArray.add(10);
+        circleArray.showAll();
     }
 
 
@@ -41,23 +46,44 @@ class CircleArray {
 
 
     public void add(int n) {
-        if(isFull()) {
+        if (isFull()) {
             System.out.println("满了");
             return;
         }
 
         arr[rear] = n;
-        rear = (rear + 1 ) % maxSize;
+        rear = (rear + 1) % maxSize;
+    }
+
+
+    public int get() {
+        if (isEmpty()) {
+            throw new RuntimeException("空的");
+        }
+        int a = (++front) % maxSize;
+        return arr[a];
     }
 
 
     public void showAll() {
-        if(isEmpty()){
+        if (isEmpty()) {
             throw new RuntimeException("数组为空");
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.printf("arr[%d]=%d\n",i,arr[i]);
+        for (int i = front; i < front + arrEffectiveSize(); i++) {
+            System.out.printf("arr[%d]=%d\n", i % maxSize, arr[i % maxSize]);
         }
+    }
+
+    /**
+     * 求出当前数组有效数据
+     */
+    public int arrEffectiveSize() {
+        // 这里需要求出循环队列的长度
+        // 那么在循环队列中会出现两种情况
+        // rear 大于 front 这种情况: rear - front
+        // rear 小于 front  分为两步计算,0 + rear 和 maxSize - front  将这两部分合成-> rear + maxSize - front
+        // 再将以上两个步骤合并 (rear + maxSize - front) %maxSize
+        return (rear + maxSize - front) % maxSize;
     }
 
     public boolean isFull() {
