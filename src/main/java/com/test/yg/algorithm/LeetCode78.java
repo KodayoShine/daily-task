c18b7a1a62c46687fccb5e743bc19c1bc901323a
shine <361596037@qq.com>
2021-03-31 21:03:08 +0800

    leetcode 78 非递归的方式,添加思路分析;回溯真的搞不懂

--- src/main/java/com/test/yg/algorithm/LeetCode78.java
+++ src/main/java/com/test/yg/algorithm/LeetCode78.java
@@ -17,19 +17,29 @@ public class LeetCode78 {
     /**
      * 遍历数组,对于数组的每一个整数,每一步都输出子集中所有子集添加这个整数,并生成新的子集
      *
+     * 第一步: 添加一个空集
+     * 第二步: 访问第一个数字的时候, 对原本的子集追加当前数值 结果 [], [x]
+     * 第三步: 再访问第二个数字的时候,在对原来的子集追加当前值y  结果 [] [x]    [y] [x, y]
+     * 第四步: 再访问第三个数字的时候,在对原来的子集追加当前值z  结果 [] [x] [y] [x, y] [z] [x, z] [y, z] [x, y, z]
+     *
      *
      * @param nums
      * @return
      */
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> arrayList = new ArrayList<>();
+        //先添加一个空的集合
         arrayList.add(new ArrayList<>());
 
         for (int i = 0; i < nums.length; i++) {
             int size = arrayList.size();
+            //每遍历一个元素就在之前子集中的每个集合追加这个元素，让他变成新的子集
             for (int j = 0; j < size; j++) {
+                //遍历之前的子集，重新封装成一个新的子集
                 List<Integer> tmp = new ArrayList<>(arrayList.get(j));
+                //然后在新的子集后面追加这个元素
                 tmp.add(nums[i]);
+                //把这个新的子集添加到集合中
                 arrayList.add(tmp);
             }
         }
