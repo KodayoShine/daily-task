025f958d8958ca5c878a7a32de330f67ebd66b5e
shine <361596037@qq.com>
2021-03-31 21:26:04 +0800

    leetcode 78 试图理解回溯

--- src/main/java/com/test/yg/algorithm/LeetCode78.java
+++ src/main/java/com/test/yg/algorithm/LeetCode78.java
@@ -5,23 +5,21 @@ import java.util.List;
 
 /**
  * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
- *
+ * <p>
  * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
- *
+ * <p>
  * 输入：nums = [1,2,3]
  * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
- *
  */
 public class LeetCode78 {
 
     /**
      * 遍历数组,对于数组的每一个整数,每一步都输出子集中所有子集添加这个整数,并生成新的子集
-     *
+     * <p>
      * 第一步: 添加一个空集
      * 第二步: 访问第一个数字的时候, 对原本的子集追加当前数值 结果 [], [x]
      * 第三步: 再访问第二个数字的时候,在对原来的子集追加当前值y  结果 [] [x]    [y] [x, y]
      * 第四步: 再访问第三个数字的时候,在对原来的子集追加当前值z  结果 [] [x] [y] [x, y] [z] [x, z] [y, z] [x, y, z]
-     *
      *
      * @param nums
      * @return
@@ -43,13 +41,52 @@ public class LeetCode78 {
                 arrayList.add(tmp);
             }
         }
-
         return arrayList;
     }
 
+    List<List<Integer>> res = new ArrayList<>();
+
+    /**
+     * 回溯算法
+     * 做每一轮的决策
+     * 一旦失败,就会退到上一次解决结果上,再次进行处理
+     *
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsets1(int[] nums) {
+        // 求不同位数的combination
+        List<Integer> cur = new ArrayList<>();
+        dfs(nums, 0, cur);
+        return res;
+    }
+
+    /**
+     * nums：原数组
+     * start：从下标start开始
+     * cur：当前得到的列表
+     */
+    private void dfs(int[] nums, int start, List<Integer> cur) {
+        System.out.println("final--" + cur);
+        // 当前位数时的cur直接加到res中，一开始是空集，后续每调用一层代表多一个位数的集合
+        res.add(new ArrayList<Integer>(cur));
+
+        // 当前位从start开始（start以前的元素在前面的位中用过了），遍历到nums末尾
+        for (int i = start; i < nums.length; ++i) {
+            cur.add(nums[i]);
+            // 当前位取nums[i]，继续从i+1开始遍历下一位，避免重复
+            dfs(nums, i + 1, cur);
+            System.out.println("dfs last--" + cur);
+            // 取消当前位的选择，下一轮循环重新选择一次当前位
+            cur.remove(cur.size() - 1);
+        }
+    }
+
+
     public static void main(String[] args) {
         LeetCode78 leetCode78 = new LeetCode78();
-        leetCode78.subsets(new int[]{1,2,3});
+        leetCode78.subsets1(new int[]{1, 2, 3});
     }
 
 
