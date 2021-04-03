b13c3151dac53c0b9c316b5df537814488168496
shine <361596037@qq.com>
2021-04-03 23:12:40 +0800

    剑指offer59.1 第二种解法

--- src/main/java/com/test/yg/algorithm/Offer59_1.java
+++ src/main/java/com/test/yg/algorithm/Offer59_1.java
@@ -1,9 +1,50 @@
 package com.test.yg.algorithm;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
  */
 public class Offer59_1 {
+
+    /**
+     * 维护一块队列,内部维护着当前滑动块内的最大值
+     * 当前滑动块超过k值,每次都将队列的首值获取到,放入到数组中
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0 || k == 0) {
+            return new int[0];
+        }
+
+        Deque<Integer> max = new LinkedList<>();
+        int[] res = new int[nums.length - k + 1];
+
+        for(int r = 0; r < nums.length; r++) {
+            while(!max.isEmpty() && max.peekLast() < nums[r]) {
+                max.removeLast();
+            }
+            max.addLast(nums[r]);
+
+            int l = r - k + 1;
+            // 删除 max 中对应的 nums[l-1]
+            if(l > 0 && max.peek() == nums[l - 1]) {
+                max.removeFirst();
+            }
+
+            // 记录窗口最大值
+            if(l >= 0) {
+                res[l] = max.peek();
+            }
+        }
+
+        return res;
+    }
+
 
     /**
      * 较为暴力的解析方式,计算每个滑动块的内的数据大小
@@ -12,7 +53,7 @@ public class Offer59_1 {
      * @param k
      * @return
      */
-    public int[] maxSlidingWindow(int[] nums, int k) {
+    public int[] maxSlidingWindow1(int[] nums, int k) {
         if (nums.length == 0 || k == 0) {
             return new int[0];
         }
@@ -25,7 +66,7 @@ public class Offer59_1 {
         return res;
     }
 
-    private int maxValue(int[] nums, int l, int r){
+    private int maxValue(int[] nums, int l, int r) {
         int max = nums[l];
         for (; l <= r; l++) {
             max = Math.max(max, nums[l]);
@@ -35,7 +76,7 @@ public class Offer59_1 {
 
     public static void main(String[] args) {
         Offer59_1 offer59_1 = new Offer59_1();
-        int[] ints = offer59_1.maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3);
+        int[] ints = offer59_1.maxSlidingWindow(new int[]{1,-1}, 1);
         for (int anInt : ints) {
             System.out.println(anInt);
         }
