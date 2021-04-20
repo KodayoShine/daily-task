ea412421dcf885090e107ef6abfd35fa172500b8
shine <361596037@qq.com>
2021-04-20 22:59:07 +0800

    783 dfs 遍历树

---
+++ src/main/java/com/test/yg/algorithm/LeetCode783.java
@@ -0,0 +1,51 @@
+package com.test.yg.algorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LeetCode783 {
+
+    static List<Integer> list = new ArrayList<>();
+
+    public int minDiffInBST(TreeNode root) {
+        dfs(root);
+        Collections.sort(list);
+        int ans = Integer.MAX_VALUE;
+        for (int i = 1; i < list.size(); i++) {
+            int cur = Math.abs(list.get(i) - list.get(i - 1));
+            ans = Math.min(ans, cur);
+        }
+        return ans;
+    }
+
+    public void dfs(TreeNode root) {
+        System.out.println(root.val);
+        list.add(root.val);
+        if (root.left != null) {
+            dfs(root.left);
+        }
+        if (root.right != null) {
+            dfs(root.right);
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
