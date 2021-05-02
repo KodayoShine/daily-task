9019d015d903cc3590f05dcb2db94ab32c24441e
shine <361596037@qq.com>
2021-05-02 22:18:59 +0800

    LeetCode68

---
+++ src/main/java/com/test/yg/algorithm/LeetCode68.java
@@ -0,0 +1,55 @@
+package com.test.yg.algorithm;
+
+/**
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ * <p>
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * <p>
+ * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ */
+public class LeetCode68 {
+
+    /**
+     * 分别递归遍历左子树，右子树，只要在子树中找到了p或q，则先找到谁就返回谁
+     * 但是如果都查不到的情况下，就说明p和q不在同一颗树内，就找root即可
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 如果树为空，直接返回null
+        if (root == null) {
+            return null;
+        }
+        // 如果 p和q中有等于 root的，那么它们的最近公共祖先即为root（一个节点也可以是它自己的祖先）
+        if (root == p || root == q) {
+            return root;
+        }
+        // 递归遍历左子树，只要在左子树中找到了p或q，则先找到谁就返回谁
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        // 递归遍历右子树，只要在右子树中找到了p或q，则先找到谁就返回谁
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // 如果在左子树中 p和 q都找不到，则 p和 q一定都在右子树中，右子树中先遍历到的那个就是最近公共祖先（一个节点也可以是它自己的祖先）
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            // 否则，如果 left不为空，在左子树中有找到节点（p或q），这时候要再判断一下右子树中的情况，如果在右子树中，p和q都找不到，则 p和q一定都在左子树中，左子树中先遍历到的那个就是最近公共祖先（一个节点也可以是它自己的祖先）
+            return left;
+        }
+
+        //当 left和 right均不为空时，说明 p、q节点分别在 root异侧, 最近公共祖先即为 root
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
