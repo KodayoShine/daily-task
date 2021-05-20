83036192b3f216a5d1a7c64e277b1233ecada22f
shine <361596037@qq.com>
2021-05-20 22:22:13 +0800

    leetcode 692 topk

---
+++ src/main/java/com/test/yg/algorithm/LeetCode692.java
@@ -0,0 +1,38 @@
+package com.test.yg.algorithm;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * 给一非空的单词列表，返回前 k 个出现次数最多的单词。
+ * <p>
+ * 返回的答案应该按单词出现频率由高到低排序。如果不同的单词有相同出现频率，按字母顺序排序。
+ * <p>
+ * 输入: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+ * 输出: ["i", "love"]
+ * 解析: "i" 和 "love" 为出现次数最多的两个单词，均为2次。
+ * 注意，按字母顺序 "i" 在 "love" 之前。
+ */
+public class LeetCode692 {
+
+    /**
+     * 依次遍历全部集合
+     * 求出每一个单词的数量
+     * 然后排序
+     *
+     * @param words
+     * @param k
+     * @return
+     */
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new LinkedHashMap();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort((o1, o2) -> o1.getValue().equals(o2.getValue()) ? o1.getKey().compareTo(o2.getKey()) : o2.getValue().compareTo(o1.getValue()));
+        return list.subList(0, k).stream().map(Map.Entry::getKey).collect(Collectors.toList());
+    }
+
+}
