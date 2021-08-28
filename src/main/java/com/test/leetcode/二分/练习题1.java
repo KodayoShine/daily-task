aad9a7c9f01607a9c982d4d32b8d38aadc52db5c
shine <361596037@qq.com>
2021-08-28 20:53:57 +0800

    二分训练  基础模型

---
+++ src/main/java/com/test/leetcode/二分/练习题1.java
@@ -0,0 +1,33 @@
+package com.test.leetcode.二分;
+
+public class 练习题1 {
+
+    /**
+     * 基础模板
+     * <p>
+     * 初始条件：left = 0, right = length-1
+     * 终止：left > right
+     * 向左查找：right = mid-1
+     * 向右查找：left = mid+1
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+}
