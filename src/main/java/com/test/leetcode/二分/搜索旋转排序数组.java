4bba46f5f2f5d48572fe8044d687d3f07725164f
shine <361596037@qq.com>
2021-08-29 23:02:06 +0800

    二分训练 搜索旋转排序数组

---
+++ src/main/java/com/test/leetcode/二分/搜索旋转排序数组.java
@@ -0,0 +1,49 @@
+package com.test.leetcode.二分;
+
+public class 搜索旋转排序数组 {
+
+    /**
+     * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+     * <p>
+     * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了旋转，
+     * 使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。
+     * <p>
+     * 例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为[4,5,6,7,0,1,2] 。
+     * <p>
+     * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回-1。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // 左大,右小
+            if (nums[mid] > nums[right]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // 左小,右大
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+}
--- src/main/java/com/test/leetcode/二分/练习题1.java
+++ src/main/java/com/test/leetcode/二分/练习题1.java
@@ -17,7 +17,7 @@ public class 练习题1 {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length;
-        while (left < right) {
+        while (left <= right) {
             int mid = (left + right) / 2;
             if (target == nums[mid]) {
                 return mid;
